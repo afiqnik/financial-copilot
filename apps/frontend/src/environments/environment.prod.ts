11cd8e00168dbcaa7d2f49302d1ca8ff4cc311e8
Nik Afiq <nikafiqdev@gmail.com>
2026-06-27 07:47:33 +0800

    update url

--- apps/frontend/src/environments/environment.prod.ts
+++ apps/frontend/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   // apiUrl: 'https://api.copilot.com/api/v1',
   // googleClientId: 'YOUR_PRODUCTION_GOOGLE_CLIENT_ID.apps.googleusercontent.com',
-  apiUrl: 'https://financial-copilot-kd6m.onrender.com/',
+  apiUrl: 'https://financial-copilot-kd6m.onrender.com/api/v1',
 };
